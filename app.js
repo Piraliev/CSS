239c396556afe4580a0333de217f7dc290811f5c
Ruslan <111184515+Piraliev@users.noreply.github.com>
2022-11-15 14:29:53 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+// 1. Переменные
+
+// camelCase >
+const firstName = 'Ruslan'; // string (const - не изменяемая переменная)
+let age = 37; // number (let - изменяемая переменная)
+const isProgrammer = true; // boolean
+
+const _ = 'private';
+const $ = 'some value';
+// const if = '' // error (нельзя использовать зарезерв. слова и символы, такие как if)
+// const 5num... = '' // error (начинать конст с цифры нельзя!)
+const num5 = 'number5';
+
+// 2. Мутирование
+
+// console.log ('Имя человека: ' + firstName + ', а возраст: ' + age); (Соединение строки с числом, в результате число приводится к строке)
+// console.log (age.toString()); // приведение к строке: .toString
+/* const lastName = prompt('Введите фамилию'); // promt (''); - ввод некой информации. 
+alert(firstName + ' ' + lastName); */ // alert(); - вывод сообщения с подтверждением. 
+
+// 3. Операторы
+
+const a = 10;
+const b = 5;
+let c = 32;
+
+// c = c + a; - эта и запись строчкой ниже равны >
+// c += a;
+// c -= a; то же, что с = с - а; 
+// c *= a; то же, что с = с * a;
+// c /= a; то же, что с = с / a;
+
+// console.log(c)
+
+// 4. Типы данных
+
+
